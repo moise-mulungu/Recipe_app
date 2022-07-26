5cd8e4e003d64fe47c36ca354ea28db5e44a09c5
moise <moisemlg90@gmail.com>
2022-07-26 07:42:44 +0200

    Create recipe add_new method

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_25_183635) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_25_191313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
+    t.string "preparation_time"
+    t.string "cooking_time"
     t.text "description"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_recipes_on_user_id"
