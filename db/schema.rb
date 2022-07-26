e522d3ca11311d7eb985e05193c58b5d8e75a14a
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-26 15:07:47 +0300

    Update migrated the recipe table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_25_191313) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_120512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_25_191313) do
     t.integer "price"
     t.integer "quantity"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_foods", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "recipe_id"
+    t.integer "food_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
