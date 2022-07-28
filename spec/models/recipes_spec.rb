7c0d581a6af8a16dcc8f953b476a5814efc46070
moise <moisemlg90@gmail.com>
2022-07-28 11:50:56 +0200

    Fix linters

--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -5,46 +5,38 @@ RSpec.describe Recipe, type: :model do
     @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to be_valid
   end
-
   it 'invalid if name is absent' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
     @recipe = @user.recipes.create(description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to_not be_valid
   end
-  
   it 'Recipe must be associated with a user' do
     @recipe = Recipe.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to_not be_valid
   end
-
   it 'valid if User is present' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
     @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to be_valid
   end
-
   it 'valid if name is present' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
     @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to be_valid
   end
-
   it 'Recipe must have a description' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
     @recipe = @user.recipes.create(name: 'Chapati', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to_not be_valid
   end
-
   it 'Recipe must have a preparation time' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
     @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', cooking_time: '30 minutes')
     expect(@recipe).to_not be_valid
   end
-
   it 'Recipe must have a cooking time' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
     @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes')
     expect(@recipe).to_not be_valid
   end
-end
-        
+end        
