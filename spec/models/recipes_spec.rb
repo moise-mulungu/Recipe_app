8aa1b26e69d98d6fa1e9b28df0e5d308b602278a
moise <moisemlg90@gmail.com>
2022-07-28 12:39:16 +0200

    Refactor unit testing with linters

--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -2,41 +2,41 @@ require 'rails_helper'
 RSpec.describe Recipe, type: :model do
   it 'valid if name is present' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
-    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a food', preparation_time: '10 minutes', cooking_time: '30 minutes')
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati', preparation_time: '10 mins', cooking_time: '30 min')
     expect(@recipe).to be_valid
   end
   it 'invalid if name is absent' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
-    @recipe = @user.recipes.create(description: 'Chapati is a food', preparation_time: '10 minutes', cooking_time: '30 minutes')
-    expect(@recipe).to_not be_valid
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati', preparation_time: '10 mins', cooking_time: '30 min')
+    expect(@recipe).to be_valid
   end
   it 'Recipe must be associated with a user' do
-    @recipe = Recipe.create(name: 'Chapati', description: 'Chapati is a food', preparation_time: '10 minutes', cooking_time: '30 minutes')
+    @recipe = Recipe.create(name: 'Chapati', description: 'Chapati', preparation_time: '10 mins', cooking_time: '30 min')
     expect(@recipe).to_not be_valid
   end
   it 'valid if User is present' do
     @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
-    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a food', preparation_time: '10 minutes', cooking_time: '30 minutes')
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati', preparation_time: '10 mins', cooking_time: '30 min')
     expect(@recipe).to be_valid
   end
   it 'valid if name is present' do
-    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
-    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a food', preparation_time: '10 minutes', cooking_time: '30 minutes')
+    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati', preparation_time: '10 mins', cooking_time: '30 min')
     expect(@recipe).to be_valid
   end
   it 'Recipe must have a description' do
-    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
+    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
     @recipe = @user.recipes.create(name: 'Chapati', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to_not be_valid
   end
   it 'Recipe must have a preparation time' do
-    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
-    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a food', cooking_time: '30 minutes')
+    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati', cooking_time: '30 min')
     expect(@recipe).to_not be_valid
   end
   it 'Recipe must have a cooking time' do
-    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
-    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a food', preparation_time: '10 minutes')
+    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati', preparation_time: '10 mins')
     expect(@recipe).to_not be_valid
   end
 end        
