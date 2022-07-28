0e254d77106bf264afc44336b9ac7869bcdb1019
moise <moisemlg90@gmail.com>
2022-07-28 11:45:00 +0200

    valid if user is present testing

--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -16,5 +16,11 @@ RSpec.describe Recipe, type: :model do
     @recipe = Recipe.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to_not be_valid
   end
+
+  it 'valid if User is present' do
+    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.com', password: 'moise123', role: 'admin')
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', preparation_time: '10 minutes', cooking_time: '30 minutes')
+    expect(@recipe).to be_valid
+  end
 end
         
