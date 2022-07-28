e83407f9ab542db25f368d033be5a69b795886ca
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-28 10:34:30 +0300

    Update to my views

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  has_many :recipes, through: :food_recipes
+  has_many :recipes, through: :recipe_foods, dependent: :destroy
 
   validates :name, presence: true
 
