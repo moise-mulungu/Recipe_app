e3f3cb487838b98b460135cd19e2123295393a0d
moise <moisemlg90@gmail.com>
2022-07-28 11:47:14 +0200

    recipe must have a preparation-time testing

--- spec/models/recipes_spec.rb
+++ spec/models/recipes_spec.rb
@@ -34,5 +34,11 @@ RSpec.describe Recipe, type: :model do
     @recipe = @user.recipes.create(name: 'Chapati', preparation_time: '10 minutes', cooking_time: '30 minutes')
     expect(@recipe).to_not be_valid
   end
+
+  it 'Recipe must have a preparation time' do
+    @user = User.create!(name: 'Moise', email: 'moisemlg90@gmail.ccom', password: 'moise123', role: 'admin')
+    @recipe = @user.recipes.create(name: 'Chapati', description: 'Chapati is a flour, onion and spices food fried in oil', cooking_time: '30 minutes')
+    expect(@recipe).to_not be_valid
+  end
 end
         
